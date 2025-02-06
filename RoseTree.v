6cbca6e4872d5af5b5757e806b18d59e576033db
Andrej Dudenhefner <mrhaandi@gmail.com>
2025-02-06 15:41:50 +0100

    refined rose_tree infrastructure

--- RoseTree.v
+++ RoseTree.v
@@ -4,49 +4,231 @@ Require Import PeanoNat.
 Require Import Nat.
 Require Import Lia.
 
-
-
 Inductive rose_tree (A : Type) : Type :=
-| node : A -> list (rose_tree A) -> rose_tree A.
+  | node : A -> list (rose_tree A) -> rose_tree A.
 
 Arguments node {A}.
 
+Fixpoint rose_tree_size {A : Type} (tr : rose_tree A) : nat :=
+  match tr with
+  | node _ ts => 1 + list_sum (map rose_tree_size ts)
+  end.
+
+(* induction principle on rose trees *)
+Lemma rose_tree_ind' {A : Type} (P : rose_tree A -> Prop) :
+  (forall (v : A) (l : list (rose_tree A)), Forall P l -> P (node v l)) ->
+  forall tr, P tr.
+Proof.
+  move=> H. elim /(Nat.measure_induction _ (@rose_tree_size A)).
+  move=> [v children] IH. apply: H.
+  elim: children IH.
+  - by constructor.
+  - move=> ?? IH IH'. constructor.
+    + apply: IH'=> /=. lia.
+    + apply: IH => /= *. apply: IH'=> /=. lia.
+Qed.
 
 (* Positions in a tree or term are lists of natural numbers. *)
 Definition position := list nat.
 
+(* fold a tree with the function f taking
+    the nodes on the path the the current node,
+    the value of the current node,
+    and the results of the children *)
+(* TODO: should the order be reversed? (fun l => f (l ++ [v])) *)
+(* TODO: should the position be an additional argument? *)
+Fixpoint fold_tree_dependent {A B : Type} (f : list A -> A -> list B -> B) (tr : rose_tree A) : B :=
+  match tr with
+  | node v children => f [] v (map (fold_tree_dependent (fun l => f (v :: l))) children)
+  end.
+
+Definition fold_tree {A B : Type} (f : A -> list B -> B) (tr : rose_tree A) : B :=
+  fold_tree_dependent (fun _ => f) tr.
+
+Definition map_tree_dependent {A B : Type} (f : list A -> A -> B) (tr : rose_tree A) : rose_tree B :=
+  fold_tree_dependent (fun l v ts => node (f l v) ts) tr.
+
+Definition map_tree {A B : Type} (f : A -> B) (tr : rose_tree A) : rose_tree B :=
+  map_tree_dependent (fun _ => f) tr.
 
 (* The depth of a tree. *)
-Fixpoint get_tdepth (A : Type) (tr: rose_tree A) :=
+Definition get_tree_depth {A : Type} (tr: rose_tree A) :=
+  fold_tree (fun _ results => list_max (map S results)) tr.
+
+Definition get_value {A : Type} (tr : rose_tree A) : A :=
   match tr with
-  | node v children => fold_left (fun acc el => let nacc := (get_tdepth A el) in
-                                                match (acc ?= nacc)  with
-                                                | Eq => acc
-                                                | Lt => nacc
-                                                | Gt => acc
-                                                end) children 0%nat
+  | node v _ => v
   end.
 
-Arguments get_tdepth {A}.
+Definition get_children {A : Type} (tr : rose_tree A) : list (rose_tree A) :=
+  match tr with
+  | node _ children => children
+  end.
 
+(* test whether the tree tr has position p *)
+Fixpoint has_position {A : Type} (tr : rose_tree A) (p : position) : bool :=
+  match p with
+  | [] => true
+  | hd :: tl =>
+    match nth_error (get_children tr) hd with
+    | Some tr' => has_position tr' tl
+    | None => false
+    end
+  end.
+
+(* The subtree of tr at legal position p. *)
+Fixpoint get_subtree {A : Type} (tr : rose_tree A) (p : position) : rose_tree A :=
+  match p with
+  | [] => tr
+  | hd :: tl =>
+    match nth_error (get_children tr) hd with
+    | Some tr' => get_subtree tr' tl
+    | None => tr
+    end
+  end.
+
+(* Give a list of node values along a path to a position *)
+(* this can be computed simpler ad hoc, but I wanted to test dependent lemmas *)
+Definition get_branch {A : Type} (tr : rose_tree A) (p : position) : list A :=
+  get_value (get_subtree (map_tree_dependent (fun l v => l) tr) p).
+
+(* map only the nth element of a list *)
+Fixpoint map_nth {A : Type} (f : A -> A) (n : nat) (l : list A) : list A :=
+  match l with
+  | [] => []
+  | hd :: tl =>
+    match n with
+    | 0 => f hd :: tl
+    | S n' => hd :: map_nth f n' tl
+    end
+  end.
+
+(* Replace the subtree of tr at position p with tr'. *)
+Fixpoint replace_subtree {A : Type} (tr' : rose_tree A) (p : position) (tr : rose_tree A) :=
+  match p with
+  | [] => tr'
+  | hd :: tl =>
+    let 'node v children := tr in
+    node v (map_nth (replace_subtree tr' tl) hd children)
+  end.
+
+Lemma replace_subtree_nil:
+  forall (A:Type) (tr : rose_tree A) tr',
+    replace_subtree tr' [] tr = tr'.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma nth_error_map_nth_eq {A : Type} (l : list A) n f :
+  nth_error (map_nth f n l) n = option_map f (nth_error l n).
+Proof.
+  elim: l n.
+  - by case.
+  - move=> ?? IH [|n]; first done.
+    by apply: IH.
+Qed.
+
+Lemma replace_get_subtree {A : Type} p (tr : rose_tree A) tr' tr'' :
+  replace_subtree tr' p tr = tr'' ->
+  has_position tr p = true ->
+  get_subtree tr'' p = tr'.
+Proof.
+  elim: p tr tr' tr''.
+  * by move=> > <-.
+  * move=> hd ? IH /= [? children] tr' [??] [] _ <- /=.
+    rewrite nth_error_map_nth_eq.
+    case E: (nth_error children hd) => [?|//].
+    by apply: IH.
+Qed.
+
+Lemma replace_subtree_decompose {A : Type} tl l l' (a:A) a' hd  tr tr'' tr3 :
+  replace_subtree tr (hd :: tl) (node a l) = node a' l' ->
+  nth_error l hd = Some tr'' ->
+  nth_error l' hd = Some tr3 ->
+  replace_subtree tr tl tr'' = tr3.
+Proof.
+  move=> [] _ <- E.
+  rewrite nth_error_map_nth_eq E.
+  by case.
+Qed.
+
+Lemma get_branch_nil {A:Type} (tr : rose_tree A) :
+  get_branch tr [] = [].
+Proof.
+  by case: tr.
+Qed.
+
+(* example of a difficult lemma for fold_tree_dependent *)
+Lemma get_branch_cons {A : Type} (tr : rose_tree A) hd (tl : position) :
+  get_branch tr (hd :: tl) =
+  match nth_error (get_children tr) hd with
+  | Some tr' => get_value tr :: get_branch tr' tl
+  | None => []
+  end.
+Proof.
+  rewrite /get_branch.
+  move: tr => [v children] /=.
+  rewrite nth_error_map.
+  case: (nth_error children hd)=> [tr' /=|//].
+  rewrite /map_tree_dependent.
+  pose f x := v :: x.
+  change (v :: ?x) with (f x).
+  elim /(@rose_tree_ind' A): tr' f tl.
+  move=> v' {}children IH f [|{}hd tl] /=; first done.
+  rewrite !nth_error_map.
+  case E: (nth_error children hd) => [? /=|//].
+  move: E => /(@nth_error_In (rose_tree A)).
+  move: IH => /Forall_forall /[apply].
+  move=> /[dup] /(_ (fun x => f (v' :: x)) tl) ->.
+  by move=> /(_ (fun x => v' :: x) tl) ->.
+Qed.
+
+Lemma get_branch_app {A : Type} (p p' : position) (tr : rose_tree A) :
+  has_position tr p = true ->
+  get_branch tr (p ++ p') = get_branch tr p ++ get_branch (get_subtree tr p) p'.
+Proof.
+  elim: p tr.
+  - move=> /= ? _.
+    by rewrite get_branch_nil.
+  - move=> ?? IH /= [??] /=.
+    move E: (nth_error _ _) => [?|//].
+    rewrite !get_branch_cons /= E.
+    by move=> /IH ->.
+Qed.
+
+Lemma get_branch_length (A : Type) (p:position) (tr : rose_tree A) :
+  has_position tr p = true ->
+  length (get_branch tr p) = length p.
+Proof.
+  elim: p tr.
+  - move=> ? /=.
+    by rewrite get_branch_nil.
+  - move=> ?? IH [??] /=.
+    move E: (nth_error _ _) => [?|//].
+    move=> /IH <-.
+    by rewrite get_branch_cons /= E.
+Qed.
+
+(* experiments, just to see if it works *)
+
+Notation term := (rose_tree (nat * nat)).
+
+(* increment all free variables by k *)
+Definition shift_term (k : nat) (t : term) : term :=
+  map_tree_dependent (fun l '(n, x) => let bound := list_sum (map fst l) in (n, if bound <=? x then k + x else x)) t.
+
+(* place term t' at position p in t *)
+Definition replace_term (t' : term) (p : position) (t : term) : term :=
+  let bound := list_sum (map fst (get_branch t p)) in
+  replace_subtree (shift_term bound t') p t.
+
+(*
 
 (* The subtree of tr at position p. *)
-Fixpoint get_subtree (A : Type) (tr : rose_tree A) (p: position) :=
-  match p with
-  | [] => Some tr
-  | hd :: tl => let 'node v children := tr
-                in match nth_error children hd with
-                   | Some tr' => get_subtree A tr' tl
-                   | None => None
-                   end
-  end.
-
-Arguments get_subtree {A}.
-
 
 (* Left-to-right iterator on trees along a position.
+now it is: fold of get_branch
 
- *)
 Fixpoint fold_left_tree (A : Type) (B : Type) (f : A -> B -> A) (tr : rose_tree B) (p : position) (aggr : A) : option A :=
   match tr, p with
   | node _ _, [] => Some aggr
@@ -56,152 +238,7 @@ Fixpoint fold_left_tree (A : Type) (B : Type) (f : A -> B -> A) (tr : rose_tree 
     | None => None
     end
   end.
-
-
-Arguments fold_left_tree {A B}.
-
-
-Lemma fold_left_tree_app : forall (A B : Type) f (p p':position) (tr tr': rose_tree B)  (a a':A),
-    get_subtree tr p = Some tr' ->
-    fold_left_tree f tr p a = Some a' ->
-    fold_left_tree f tr (p++p') a = fold_left_tree f tr' p' a'.
-Proof.
-  induction p.
-  * intros.
-    simpl in *.
-    destruct tr.
-    congruence.
-  * intros.
-    simpl in H.
-    simpl in H0.
-    simpl.               
-    destruct tr.
-    destruct (nth_error l a) eqn:Nth.
-    ** eapply IHp in H;eauto.
-    ** congruence.
-Qed.
-
-Lemma fold_left_tree_S_k :
-  forall (A : Type)   (p:position) (tr tr' : rose_tree A) (k : nat),
-    get_subtree tr p = Some tr' ->
-    fold_left_tree (fun x _ => S x) tr p k = Some (k + (length p)).
-Proof.
-  induction p.
-  * intros.
-    simpl in *.
-    destruct tr. 
-    now replace (k+0) with k by lia.
-  * intros.
-    destruct tr.
-    simpl in *.
-    destruct (nth_error l a); try congruence.
-    replace (k + S (length p)) with ((S k) + (length p)) by lia.
-    eauto.
-Qed.
-
-Lemma fold_left_tree_S_0 :
-  forall (A : Type)   (p:position) (tr tr' : rose_tree A),
-    get_subtree tr p = Some tr' ->
-    fold_left_tree (fun x _ => S x) tr p 0 = Some (length p).
-Proof.
-  intros.
-  eauto using fold_left_tree_S_k.
-Qed.
-
-
-
-
-(* Left-to-right iterator on trees along a position.
-
  *)
-Fixpoint map_tree (A : Type) (B : Type) (f : A -> B) (tr : rose_tree A)  : rose_tree B :=
-  match tr with
-  | node v ts => node (f v) (map (map_tree A B f) ts)
-  end.
-
-Arguments map_tree {A B}.
-
-
-
-
-(* The replace in the tree tr the subtree at position p with tr'. *)
-Fixpoint replace_subtree (A : Type) (tr : rose_tree A) (p: position) (tr' : rose_tree A) :=
-  match p with
-  | [] => tr'
-  | hd :: tl => let 'node v children := tr in
-                let pairs := combine (seq 0 (length children)) children in
-                let mapping := fun  '(m, tr'') =>
-                                 if m =? hd
-                                 then replace_subtree A tr'' tl tr'
-                                 else tr'' in
-                node v (map mapping pairs)
-  end.
-
-Arguments replace_subtree {A}.
-
-Lemma replace_subtree_nil:
-  forall (A:Type) (tr : rose_tree A) tr',
-    replace_subtree tr [] tr' = tr'.
-Proof.
-  intros.
-  simpl.
-  destruct tr.
-  now simpl.
-Qed.
-
-
-Lemma nth_error_combine_map:
-  forall A B C D (f : A -> B) (f' : (C*A) -> D) (l : list A) (l': list C) a e e',
-    nth_error l a = Some e ->
-    nth_error l' a = Some e' ->
-    length l' = length l ->
-    nth_error (map f' (combine l' l)) a = Some (f' (e',e)).
-Proof.
-  induction l.
-  * intros.
-    simpl in *.
-    simpl.
-    apply length_zero_iff_nil in H1.
-    subst l'.
-    simpl in *.
-    rewrite nth_error_nil in H.
-    congruence.
-  * intros.
-    destruct l'; simpl in H1;try congruence.
-    destruct a0.
-    ** simpl.
-       simpl in H, H0.
-       congruence.
-    ** simpl in H, H0.
-       simpl.
-       eapply IHl in H;eauto.
-Qed.
-
-Lemma replace_subtree_decompose:
-  forall A tl l l' (a:A) a' hd  tr tr'' tr3,
-    replace_subtree (node a l) (hd :: tl) tr = node a' l' ->
-    nth_error l hd = Some tr'' ->
-    nth_error l' hd = Some tr3 ->
-    replace_subtree tr'' tl tr = tr3.
-Proof.
-  admit.
-Admitted.
-
-  
-Lemma replace_get_subtree:
-  forall (A:Type)  p (tr : rose_tree A) tr' tr'' tr''',
-    replace_subtree tr p tr' = tr'' ->
-    get_subtree tr p = Some tr''' ->
-    get_subtree tr'' p = Some tr'.
-Proof.
-  induction p.
-  * intros.
-    destruct tr.
-    simpl in *.
-    now subst tr'.
-  * intros.
-    admit.
-Admitted.
 
 
 (*
